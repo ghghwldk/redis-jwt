b119e838e04e84353aa3fa02f4c42961ffd51019
ghghwldk <ghghwldk@gmail.com>
2024-06-07 22:06:52 +0900

    first commit

---
+++ src/main/java/com/tutorial/jwtsecurity/util/SecurityUtil.java
@@ -0,0 +1,25 @@
+package com.tutorial.jwtsecurity.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Slf4j
+public class SecurityUtil {
+
+    private SecurityUtil() { }
+
+    public static Long getCurrentMemberId() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || authentication.getName() == null) {
+            throw  new RuntimeException("Security Context 에 인증 정보가 없습니다.");
+        }
+
+        return Long.parseLong(authentication.getName());
+    }
+}
