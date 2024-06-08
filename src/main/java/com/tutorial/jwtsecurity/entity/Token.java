5ec5781d58c09520d485c692dd0b8051ea80a464
ghghwldk <ghghwldk@gmail.com>
2024-06-08 16:50:19 +0900

    source code info changed(deleted or updated)

--- src/main/java/com/tutorial/jwtsecurity/entity/Token.java
+++ src/main/java/com/tutorial/jwtsecurity/entity/Token.java
@@ -22,6 +22,7 @@ public class RefreshToken {
     @Column(name = "rt_value")
     private String value;
 
+
     @Builder
     public RefreshToken(String key, String value) {
         this.key = key;
--- src/main/java/com/tutorial/jwtsecurity/repository/TokenRepository.java
+++ src/main/java/com/tutorial/jwtsecurity/repository/TokenRepository.java
@@ -4,9 +4,10 @@ import com.tutorial.jwtsecurity.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
     Optional<RefreshToken> findByKey(String key);
 }
